9639c8722dce857b02aefceff00326f8fe766121
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-01-17 22:30:18 +0630

    suspense and streaming

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,17 +1,14 @@
 import PostsList from "@/components/posts/PostsList";
+import { Suspense } from "react";
 
 const page = async () => {
-  // with server components, u can make a fetch call without useEffect
-  // this can be closer to actual data source (close to database)
-
-  const response = await fetch(`https://dummyjson.com/posts?limit=3`);
-  const data = await response.json();
-
   return (
     <main className="text-center pt-16 px-5">
       <h1 className="text-5xl font-semibold mb-7">All posts</h1>
 
-      <PostsList posts={data.posts} />
+      <Suspense fallback="Loading...">
+        <PostsList />
+      </Suspense>
     </main>
   );
 };
--- components/posts/PostsList.tsx
+++ components/posts/PostsList.tsx
@@ -5,10 +5,20 @@ type Props = {
   posts: Post[];
 };
 
-const PostsList = ({ posts }: Props) => {
+// ({ posts }: Props)
+
+const PostsList = async () => {
+  // with server components, u can make a fetch call without useEffect
+  // this can be closer to actual data source (close to database)
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const response = await fetch(`https://dummyjson.com/posts?limit=3`);
+  const data = await response.json();
+
   return (
     <ul>
-      {posts.map((post) => (
+      {data.posts.map((post: Post) => (
         <li key={post.id} className="max-w-[400px] mb-3 mx-auto">
           <Link href={`/posts/${post.id}`}>{post.title}</Link>
         </li>
